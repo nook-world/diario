e7099c06b3d75cb8ea9fae80218920fdc5d9871c
Diogo Ferreira Fernandes <dio@mac.local>
2020-04-09 11:44:17 -0300

    Offline support

--- next.config.js
+++ next.config.js
@@ -1,31 +1,7 @@
-const withOffline = require('next-offline');
-
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: '/service-worker.js',
-          destination: '/_next/static/service-worker.js',
-        },
-      ]
-    },
-  },
+const withPWA = require('next-pwa');
+ 
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
 });
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,11 +11,14 @@ function Layout({
     <div className={ [background, 'layout'].join(' ') }>
       <Head>
         <title>My Miles · Animal Crossing daily tasks</title>
+        <meta name='description' content='Create your milestones on Animal Crossing New Horizons' />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="msapplication-TileColor" content="#5bbad5" />
@@ -23,7 +26,7 @@ function Layout({
       </Head>
       { children }
       <Menu />
-      <div class="ios-peek" />
+      <div className="ios-peek" />
     </div>
   );
 }
