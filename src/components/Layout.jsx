0f066cc93555c708d7f6063b549a76c457438053
Diogo Ferreira Fernandes <dio@mac.local>
2020-04-10 07:44:30 -0300

    Add custom document to add lang, add some missing headers, creste custom 404

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,13 +11,17 @@ function Layout({
     <div className={ [background, 'layout'].join(' ') }>
       <Head>
         <title>My Miles · Animal Crossing daily tasks</title>
+        <meta name='apple-mobile-web-app-title' content="My Miles" />
         <meta name='description' content='Create your milestones on Animal Crossing New Horizons' />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel='shortcut icon' href='/icons/favicon-32x32.png' />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta charSet='utf-8' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='format-detection' content='telephone=no' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+function Page() {
+  return (
+    <Layout>
+      <h1>Error 404</h1>
+      <p style={{ margin: 'auto', fontSize: '2em' }}><b>Ops, page not found :(</b></p>
+    </Layout>
+  );
+}
+
+export default Page;
